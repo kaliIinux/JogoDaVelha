bf9873884b1ed8f026078713ad886402801b5e74
kaliIinux <125105262+kaliIinux@users.noreply.github.com>
2023-03-30 10:37:15 -0300

    Add files via upload

--- Jogo_da_Velha/main.py
+++ Jogo_da_Velha/main.py
@@ -11,6 +11,18 @@ board = [[1,2,3], [4,5,6], [7,8,9]]
     []
 ]
 
+topic = Nome da sala do jogo!
+    est
+
+topic = match_jogadores
+    ets/matchplayers
+
+topic = ganhador
+    ets/vencedor
+
+topic = empate
+    ets/empate
+    
 player_1 = 'X' / pos = 1
 if pos in board
 """
