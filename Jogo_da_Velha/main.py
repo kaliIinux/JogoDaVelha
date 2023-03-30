4f99d8b02e82d4cf29bb4d759772e76371ce49de
kaliIinux <125105262+kaliIinux@users.noreply.github.com>
2023-03-30 10:31:26 -0300

    Add files via upload

---
+++ Jogo_da_Velha/main.py
@@ -0,0 +1,16 @@
+"""
+Documentação
+Parâmetro Opcional
+GEtters/Setters como properties
+Atributos privados
+Métodos privados
+
+Jogo da velha
+board = [[1,2,3], [4,5,6], [7,8,9]]
+[
+    []
+]
+
+player_1 = 'X' / pos = 1
+if pos in board
+"""
